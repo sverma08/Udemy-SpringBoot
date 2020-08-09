4345039ad5eee379ad732b00a773da182ac62649
sverma08 <saurav.verma97@gmail.com>
2020-08-09 18:06:13 +0530

    Path variables and Path parameters

--- src/main/java/com/example/ui/controller/UserController.java
+++ src/main/java/com/example/ui/controller/UserController.java
@@ -1,28 +1,45 @@
 package com.example.ui.controller;
 
+import com.example.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
-    @GetMapping
-    public String getUser() {
-        return "Get User was called!";
+    @GetMapping()
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+                          @RequestParam(value = "limit", defaultValue = "50") int limit) {
+        return "Get User was called with page = " + page + " and limit = " + limit;
+    }
+
+    @GetMapping(path = "/{userid}")
+    public UserRest getUser(@PathVariable String userid) {
+        UserRest returnValue = new UserRest();
+
+        returnValue.setFirstName("Saurav");
+        returnValue.setLastName("Verma");
+        returnValue.setEmail("saurav.verma97@gmail.com");
+        returnValue.setUserid(userid);
+
+        return returnValue;
     }
 
     @PostMapping
     public String createUser() {
+
         return "Create User was called!";
     }
 
     @PutMapping
     public String updateUser() {
+
         return "Update User was called!";
     }
 
     @DeleteMapping
     public String deleteUser() {
+
         return "Delete User was called!";
     }
 }
